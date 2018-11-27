417ce25b138285198d0d1de3d158864cc57f8219
Sean <Seanmcfarlane97@gmail.com>
2018-11-27 22:09:34 +1100

    Added visualisation of sectors

--- Level Generation Test/Assets/Scripts/BSPGeneration.cs
+++ Level Generation Test/Assets/Scripts/BSPGeneration.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BSPGeneration : MonoBehaviour {
+public class BSPGeneration : MonoBehaviour
+{
     public int rows, columns;
     public int minRoomSize, maxRoomSize;
 
     public static List<Rect> sectionList = new List<Rect>();
+    public static List<Rect> roomList = new List<Rect>();
     public static List<Rect> corridorList = new List<Rect>();
-
-
 
     public GameObject floorTile;
     public GameObject[,] floorPositions;
 
-
+    int counter = 0;
 
     private void Start()
     {
@@ -24,15 +24,41 @@ public class BSPGeneration : MonoBehaviour {
 
         floorPositions = new GameObject[rows, columns];
 
-       // DrawRooms(initialSection);
+        // DrawRooms(initialSection);
 
     }
 
     private void OnDrawGizmos()
     {
+        float mostLeftSection = 5;
+        float mostRightSection = 0;
+        bool notRun = true;
+        Rect leftMost = new Rect(-1, -1, 0, 0); // i.e null
+        Rect rightMost = new Rect(-1, -1, 0, 0); // i.e null
+
+        /*if (notRun)
+        {
+            foreach(Rect rect in sectionList)
+            {
+                if (rect.y < mostLeftSection) { leftMost = rect; }
+                if (rect.y > mostRightSection) { rightMost = rect; }
+                counter++;
+                if (counter >= sectionList.Count) { notRun = false; }
+            }
+        }
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(rightMost.center, rightMost.size);
+        Gizmos.DrawCube(leftMost.center, leftMost.size);*/
+
+
         foreach (Rect rect in sectionList)
         {
-            Gizmos.color = new Color(0, 1, 0, 0.5f); 
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            Gizmos.DrawWireCube(rect.center, rect.size);
+        }
+        foreach (Rect rect in roomList)
+        {
+            Gizmos.color = new Color(0, 1, 0, 0.5f);
             Gizmos.DrawCube(rect.center, rect.size);
         }
         foreach (Rect rect in corridorList)
@@ -44,19 +70,19 @@ public class BSPGeneration : MonoBehaviour {
 
     public void DrawRooms(Section section)
     {
-        if(section == null)
+        if (section == null)
         {
             return;
         }
-        if(section.IsLeaf())
+        if (section.IsLeaf())
         {
-            for(int i = (int)section.room.x; i < section.room.xMax; i++)
+            for (int i = (int)section.room.x; i < section.room.xMax; i++)
             {
                 for (int j = (int)section.room.y; j < section.room.yMax; j++)
                 {
                     GameObject instance = Instantiate(floorTile, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
                     instance.transform.SetParent(transform);
-    
+
                 }
             }
         }
@@ -79,11 +105,11 @@ public class BSPGeneration : MonoBehaviour {
 
         public void CreateRoom()
         {
-            if(left != null)
+            if (left != null)
             {
                 left.CreateRoom();
             }
-            if(right != null)
+            if (right != null)
             {
                 right.CreateRoom();
             }
@@ -95,9 +121,6 @@ public class BSPGeneration : MonoBehaviour {
                 int roomY = (int)Random.Range(1, rect.height - roomHeight - 1);
 
                 room = new Rect(rect.x + roomX, rect.y + roomY, roomWidth, roomHeight);
-
-                print("xMin: " + rect.xMin + "  xMax: " + rect.xMax);
-
 
                 float corridorX = Random.Range(room.xMin + 1, room.xMax - 1);
                 float corridorY;
@@ -111,17 +134,14 @@ public class BSPGeneration : MonoBehaviour {
                 }
                 corridor = new Rect(corridorX, corridorY, 1, 1);
 
-                sectionList.Add(room);
+                roomList.Add(room);
                 corridorList.Add(corridor);
-
-
-                Debug.Log("Created room " + room + " in section " + sectionID + " " + rect);
             }
         }
 
         public void CreateCorridor()
         {
-            
+
         }
 
         public bool IsLeaf()
@@ -160,7 +180,6 @@ public class BSPGeneration : MonoBehaviour {
 
             if (Mathf.Min(rect.height, rect.width) / 2 < minRoomSize)
             {
-                Debug.Log("Section " + sectionID + " will be a Leaf");
                 return false;
             }
 
@@ -170,6 +189,8 @@ public class BSPGeneration : MonoBehaviour {
 
                 left = new Section(new Rect(rect.x, rect.y, rect.width, splitValue));
                 right = new Section(new Rect(rect.x, rect.y + splitValue, rect.width, rect.height - splitValue));
+                sectionList.Add(left.rect);
+                sectionList.Add(right.rect);
             }
             else
             {
@@ -177,8 +198,8 @@ public class BSPGeneration : MonoBehaviour {
 
                 left = new Section(new Rect(rect.x, rect.y, splitValue, rect.height));
                 right = new Section(new Rect(rect.x + splitValue, rect.y, rect.width - splitValue, rect.height));
-                //sectionList.Add(left.rect);
-                //sectionList.Add(right.rect);
+                sectionList.Add(left.rect);
+                sectionList.Add(right.rect);
             }
             return true;
         }
@@ -186,9 +207,7 @@ public class BSPGeneration : MonoBehaviour {
 
     public void Partition(Section section)
     {
-        Debug.Log("Splitting section " + section.sectionID + ": " + section.rect);
         if (section.IsLeaf() == true)
-        //If the section is too large
         {
             if (section.rect.width > maxRoomSize
                 || section.rect.height > maxRoomSize
@@ -197,10 +216,6 @@ public class BSPGeneration : MonoBehaviour {
 
                 if (section.Split(minRoomSize, maxRoomSize))
                 {
-                    Debug.Log("Split section " + section.sectionID + " in "
-                              + section.left.sectionID + ": " + section.left.rect + ", "
-                              + section.right.sectionID + ": " + section.right.rect);
-
                     Partition(section.left);
                     Partition(section.right);
                 }
